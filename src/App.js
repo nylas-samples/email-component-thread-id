f2ea4121709e320710fb85c140831193c204d415
tjperry07 <tjperry07@users.noreply.github.com>
2022-04-12 14:19:14 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import "@nylas/components-email";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Nylas Component Thread ID Example</h1>
+      <nylas-email
+        id="759a1f79-0467-4fd1-8f02-9384bd2ff31f"
+        thread_id="we7ml80n6x9hjnlsohs2v658"
+      >
+        {" "}
+      </nylas-email>
+    </div>
+  );
+}
